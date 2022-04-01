ed62bcb5cdda6d91cbb007669d5262c17a047217
Mr Zoom <mr.zoom.orsk@gmail.com>
2022-04-01 21:59:20 +0500

    Changing for Heroku

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_restful import Api
 from data import db_session
 from data import users_resource
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -12,7 +13,8 @@ def main():
     db_session.global_init('db/cinema.db')
     api.add_resource(users_resource.UsersListResource, '/api/v2/users')
     api.add_resource(users_resource.UsersResource, '/api/v2/users/<int:user_id>')
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
 if __name__ == '__main__':
