0f26be0e51f61310d9b774a7d15ef826581f6191
Mr Zoom <mr.zoom.orsk@gmail.com>
2022-04-01 23:55:43 +0500

    Added routes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_restful import Api
 from data import db_session
-from data import users_resource
+from data import users_resource, cinemas_resource, halls_resource, logs_resource
 import os
 
 app = Flask(__name__)
@@ -13,6 +13,12 @@ def main():
     db_session.global_init('db/cinema.db')
     api.add_resource(users_resource.UsersListResource, '/api/v2/users')
     api.add_resource(users_resource.UsersResource, '/api/v2/users/<int:user_id>')
+    api.add_resource(halls_resource.HallsListResource, '/api/v2/halls')
+    api.add_resource(halls_resource.HallsResource, '/api/v2/halls/<int:hall_id>')
+    api.add_resource(cinemas_resource.CinemasListResource, '/api/v2/cinemas')
+    api.add_resource(cinemas_resource.CinemasResource, '/api/v2/cinemas/<int:cinema_id>')
+    api.add_resource(logs_resource.LogsListResource, '/api/v2/logs')
+    api.add_resource(logs_resource.LogsResource, '/api/v2/logs/<int:log_id>')
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
 
